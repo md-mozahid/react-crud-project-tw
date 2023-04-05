4eedee330c90648757fffd3da8c1969effa3cb31
mozahid <ce.muzahid@gmail.com>
2023-04-05 11:40:38 +0600

    add new branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import ContactForm from './contacts/ContactForm'
 import Contacts from './contacts/Contacts'
-// import AddContact from './pages/AddContact'
-// import Navbar from './layouts/Navbar'
 
 function App() {
   return (
     <>
-      {/* <Navbar /> */}
       <ContactForm />
-      {/* <AddContact /> */}
-
       <Contacts />
     </>
   )
